cb7bfe6410f7a8f0ee42ebfcaa7d28c5ce8a3eaa
DESKTOP-R47VUQ7\ALEXANDR <mos3ten@gmail.com>
2016-09-14 20:48:59 +0300

    no message

---
+++ math/math/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace math
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double l;
+            double r;
+            float a;
+            float b;
+            float c;
+            float d;
+
+            Console.WriteLine("Please, enter the radius of circle:");
+
+            r = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Please,enter three numbers. Each new from a new line.");
+
+            a = float.Parse(Console.ReadLine());
+            b = float.Parse(Console.ReadLine());
+            c = float.Parse(Console.ReadLine());
+
+            l = r*(2 * 3.14);
+
+            d = (a + b + c) / 3;
+
+            Console.WriteLine("The circumference is {0}",l);
+            Console.WriteLine("The middle ariphmetic is {0}",d);
+
+            Console.ReadKey();
+            
+
+        }
+    }
+}
