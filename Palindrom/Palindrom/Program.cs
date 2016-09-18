a9b4bfeb9fb47499b2d3f80447ee6e1cd75e1dc0
DESKTOP-R47VUQ7\ALEXANDR <mos3ten@gmail.com>
2016-09-18 11:33:43 +0300

    no message

---
+++ Palindrom/Palindrom/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Palindrom
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string word;
+            int i=0;
+            
+            Console.WriteLine("Please,enter the word:");
+            word = Console.ReadLine();
+            
+            if (word[i] == word[word.Length - i-1])
+                {
+                for (i = 0; i < word.Length; i++)
+                {
+                    Console.WriteLine(word[word.Length - i - 1]);
+                }
+                Console.WriteLine("Entered word is palindrom");
+                }
+          
+            else
+             {
+                 Console.WriteLine("The enterde word isn't a palindrom");
+             }
+
+            Console.ReadKey();
+        }
+    }
+}
