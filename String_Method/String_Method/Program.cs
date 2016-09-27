11f2c2e3d9be73544deaa32e7a3090f2265b1a05
DESKTOP-R47VUQ7\ALEXANDR <mos3ten@gmail.com>
2016-09-27 18:33:03 +0300

    no message

---
+++ String_Method/String_Method/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace String_Method
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please,enter the text messagge and parameters for method:");
+            string messagge = Console.ReadLine();
+            stringOut(messagge);
+        }
+        static void stringOut(string messagge,params int[] List)
+        {
+             if (List.Length > 20)
+                {
+                    Console.WriteLine(messagge);
+                }
+                else Console.WriteLine("Sorry,but the total average of parameters is not enough. Its total average now is:{0}",List.Length);                
+        }
+    }
+}
